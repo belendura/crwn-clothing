adc55c67679a5eecc5a32d1c55572ae194b13592
Belen Dura <belen.dura@gmail.com>
2019-07-26 18:34:57 +0200

    adding last changes, normalization and collections components

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -26,6 +26,6 @@ export const removeItemFromCart = (cartItems, cartItemToRemove)=>{
         : cartItem)
     } 
     else{
-     return cartItems.filter(cartItem => cartItem.id != cartItemToRemove.id)
+     return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
     } 
 }
